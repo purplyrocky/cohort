d3f3c77bb61f6ebace9b812ea59dccf6c7a2f183
purplyrocky <purplyrocky@gmail.com>
2015-07-06 16:20:43 -0700

    added second exercise

--- dayOne/secondex/js/script.js
+++ dayOne/secondex/js/script.js
@@ -1,18 +1,18 @@
-//Ask the user their name
-//Ask the user their email
-//Ask the user their favorite movie
-//Assign all the values prompted for into an object
-//Log the object to the console
+//Prompt for a number value (provide a default of 10)
+//Prompt for another number value (provide a default of 10)
+//Convert the promped values into integers.
+//Make sure that the input from steps 1 and 2 are numbers.
+//If they are numbers, add the numbers together and log the value to the console.
+//If either of the prompted values are not numbers, alert someting to the user that they must input nubmers only.
 
-var whoDat = prompt('enter your name'),
-    yourEmail = prompt('enter your email'),
-    yourMovie = prompt('enter your favorite movie'),
-    disFoo = {
-      name: whoDat,
-      email: yourEmail,
-      movie: yourMovie
-    };
-    console.log(disFoo);
-	document.querySelector('.results').innerHTML = '<ul><li>' + disFoo.name + '</li>' +
-													'<li>' + disFoo.email + '</li>' +
-													'<li>' + disFoo.movie + '</li></ul>';
+var num1 = prompt('enter a number'),
+    parsed1 = parseInt(num1, 10),
+    num2 = prompt('enter another number'),
+    parsed2 = parseInt(num2, 10),
+    answer = parsed1 + parsed2;
+
+    if(!isNaN(parsed1) && !isNaN(parsed2)){
+      console.log(answer);
+    } else {
+      alert('you did not enter a number');
+    }
