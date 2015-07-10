894a166e0a5b9c23e0967c0dd702e5e4aab7a7d3
purplyrocky <purplyrocky@gmail.com>
2015-07-10 10:33:10 -0700

    made li alert title

--- dayfour/js/script.js
+++ dayfour/js/script.js
@@ -1,16 +1,15 @@
 var movies = [
-	['Lion King', '89 min', '1994'],
-	['Men in Black', '98 min', '1997'],
-	['Harry Potter and the Prisoner of Azkaban', '142 min', '2004'],
-	['Lilo and Stitch', '85 min', '2002'],
-	['X-Men First Class', '132 min', '2011'],
-	['Anchorman', '94 min', '2004'],
-	['Hot Rod', '88 min', '2007'],
-	['Bride Wars', '89 min', '2009'],
-	['Princess Diaries', '115 min', '2001'],
-	['Hellboy', '122 min', '2004']
+    ['Lion King', '89 min', '1994'],
+    ['Men in Black', '98 min', '1997'],
+    ['Harry Potter and the Prisoner of Azkaban', '142 min', '2004'],
+    ['Lilo and Stitch', '85 min', '2002'],
+    ['X-Men First Class', '132 min', '2011'],
+    ['Anchorman', '94 min', '2004'],
+    ['Hot Rod', '88 min', '2007'],
+    ['Bride Wars', '89 min', '2009'],
+    ['Princess Diaries', '115 min', '2001'],
+    ['Hellboy', '122 min', '2004']
 ];
-
 function e(nodeType, text, attributes, styles, selector){
     var element = document.createElement(nodeType);
         element.textContent = text;
@@ -32,4 +31,7 @@ function e(nodeType, text, attributes, styles, selector){
 }
 for(i = 0; i < movies.length; i++){
     var li = e("li", movies[i][0], {}, {}, '#ul');
+    li.addEventListener('click', function(){
+        alert(this.textContent);
+    });
 }
